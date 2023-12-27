ae98b797ca7188adc638b67b075d9dad9f3bcc01
gopinathan123 <gopinathan019@gmail.com>
2023-12-27 15:30:25 +0530

    hi

--- test/specs/Assignments22-12-2023/scrollexample.js
+++ test/specs/Assignments22-12-2023/scrollexample.js
@@ -3,6 +3,6 @@ describe("Scroll",()=>{
         await browser.url("https://www.amazon.in/");
         await browser.maximizeWindow();
         await $("//a[text()='Mobiles']").click();
-        
+        await $("//div[@cel_widget_id='acsux-widgets_content-grid_merchandised-search-11_row5-col3']").click();
     })
 })
